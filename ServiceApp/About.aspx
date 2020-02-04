8bb006c01a3a46b10fc0755bf3aaeae9c5e7672c
Kelvin Lee <kahokelvinlee@gmail.com>
2020-02-03 21:39:38 -0800

    Added customer upload form

--- ServiceApp/About.aspx
+++ ServiceApp/About.aspx
@@ -1,7 +1,23 @@
-﻿<%@ Page Title="About" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="ServiceApp.About" %>
+﻿<%@ Page Title="Customer" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="About.aspx.cs" Inherits="ServiceApp.About" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-    <h2><%: Title %>.</h2>
-    <h3>Your application description page.</h3>
-    <p>Use this area to provide additional information.</p>
+    <h1>Customer</h1>
+    <br />
+    <asp:Label ID="nameLabel" runat="server" Text="Name:"></asp:Label><br />
+    <asp:TextBox ID="nameTextBox" runat="server"></asp:TextBox><br />
+    <asp:Label ID="addressLabel" runat="server" Text="Address:"></asp:Label><br />
+    <asp:TextBox ID="addressTextBox" runat="server"></asp:TextBox>
+    <br />
+    <br />
+    <asp:Label ID="maleRadioLbl" runat="server" Text="Male:"></asp:Label><asp:RadioButton ID="maleRadioBtn" GroupName="gender" runat="server" />
+    <asp:Label ID="femaleRadioLbl" runat="server" Text="Female:"></asp:Label><asp:RadioButton ID="femaleRadioBtn" GroupName="gender" runat="server" />
+    <asp:Calendar ID="birthCalendar" runat="server"></asp:Calendar>
+    <br />
+    <asp:Label ID="imageLabel" runat="server" Text="Image:"></asp:Label><asp:FileUpload ID="FileUpload1" runat="server" />
+    <br />
+    <asp:Button ID="UploadBtn" runat="server" Text="Submit" OnClick="UploadBtn_Click" />
+    <br />
+    <br />
+    <asp:Label ID="Label1" runat="server"></asp:Label>
+
 </asp:Content>
--- ServiceApp/About.aspx.cs
+++ ServiceApp/About.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.Odbc;
+using System.IO;
 
 namespace ServiceApp
 {
@@ -12,6 +14,54 @@ namespace ServiceApp
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        protected void UploadBtn_Click(object sender, EventArgs e)
+        {
+            if(FileUpload1.HasFile)
+            {
+                // Image src
+                //String newFileName = "C:/Users/A01048343/Desktop/Images/octo.jpg";
+                // //String DestinationLoc = "C:\Users\A01048343\Desktop\Images";
+
+                //Reading from an image file
+                //FileStream fs = File.OpenRead(newFileName);
+                //Byte[] byData = new byte[fs.Length - 1];
+                Byte[] byData = FileUpload1.FileBytes;
+                //fs.Read(byData, 0, byData.Length);
+
+                //inserting to RDBMS via ODBC
+                try
+                {
+                    // Connection
+                    string connectionString = "DSN=mySqlServer;Uid=SYSTEM;Pwd=password";
+                    OdbcConnection conn = new OdbcConnection();
+                    conn = new OdbcConnection(connectionString);
+                    conn.Open();
+                    string query = "INSERT INTO Customer (ID, Name, Address, Birthdate, Gender, Picture) VALUES (?, ?, ?, ?, ?, ?)";
+                    OdbcCommand exe = new OdbcCommand(query, conn);
+                    //exe.Parameters.AddWithValue("@guidValue", Guid.NewGuid().ToString("D"));
+                    exe.Parameters.Add("@ID", OdbcType.Text).Value = Guid.NewGuid().ToString("D");
+                    exe.Parameters.Add("@Name", OdbcType.Text).Value = nameTextBox.Text;
+                    exe.Parameters.Add("@Address", OdbcType.Text).Value = addressTextBox.Text;
+                    exe.Parameters.Add("@Birthdate", OdbcType.DateTime).Value = birthCalendar.SelectedDate.ToShortDateString();
+                    exe.Parameters.Add("@Gender", OdbcType.Text).Value = maleRadioBtn.Checked ? "M" : "F";
+                    exe.Parameters.Add("@Picture", OdbcType.Image, byData.Length).Value = byData;
+                    if (exe.ExecuteNonQuery() > 0)
+                    {
+                        conn.Close();
+                    }
+                    Label1.Text = "Customer added";
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err.Message);
+                }
+            }
+            else
+            {
+                Label1.Text = "Customer was not added";
+            }
         }
     }
 }
