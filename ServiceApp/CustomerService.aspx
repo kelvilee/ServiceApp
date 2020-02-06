a668bdae7006db8f3bc266f2633d630003275670
evanmorrow <evanmorrow@gmail.com>
2020-02-05 21:43:35 -0800

    work in progress just need to pull.

---
+++ ServiceApp/CustomerService.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CustomerService.aspx.cs" Inherits="ServiceApp.CustomerService" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Button ID="Button1" runat="server" Text="Add" />
+        </div>
+        <div> 
+        <asp:ListView ID="ListView2" runat="server" OnSelectedIndexChanged="ListView1_SelectedIndexChanged">
+            <ItemTemplate>
+                <asp:Label runat="server" Text='<%# Eval("customerid") %>'> </asp:Label>
+                <asp:Label runat="server" Text='<%# Eval("ServiceTypeID") %>'></asp:Label> 
+                <asp:Label runat="server" Text='<%# Eval("ExpectedDuration") %>'></asp:Label> 
+                <br> </br>
+<%--                <asp:LinkButton ID="SelectButton" runat="server" CommandName="Select" Text='<% #Eval("CustomerID") %>' />  --%>        
+            </ItemTemplate>
+        </asp:ListView>
+     </div> 
+    </form>
+</body>
+</html>
---
+++ ServiceApp/CustomerService.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Security;
+using System.Collections;
+using System.Data;
+using System.Data.Odbc;
+
+namespace ServiceApp
+{
+    public partial class CustomerService : System.Web.UI.Page
+    {
+        protected void Page_Init(object sender, EventArgs e)
+        {
+            ListView2.ItemCommand += new EventHandler<ListViewCommandEventArgs>(ListView1_ItemCommand);
+        }
+        void ListView1_ItemCommand(object sender, ListViewCommandEventArgs e)
+        {
+            //Literal1.Text = "You clicked the " + (String)e.CommandName + " button";
+        }
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string myConnection = "dsn=myOracle;uid=system;pwd=oracle1";
+            OdbcConnection myConn = new OdbcConnection(myConnection);
+            myConn.Open();
+            string mySelectQuery = "select utl_raw.cast_to_varchar2(UTL_RAW.cast_to_raw(CustomerID)) as CustomerID, utl_raw.cast_to_varchar2(UTL_RAW.cast_to_raw(ServiceTypeID)) as ServiceTypeID, ExpectedDuration from customerservice;";
+            OdbcCommand command = new OdbcCommand(mySelectQuery, myConn);
+
+            if (!Page.IsPostBack)
+            {
+                ListView2.DataSource = command.ExecuteReader();
+                ListView2.DataBind();
+            }
+            myConn.Close();
+        }
+
+        protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Console.WriteLine("testing in selectedindexchanged.");
+        }
+    }
+}
