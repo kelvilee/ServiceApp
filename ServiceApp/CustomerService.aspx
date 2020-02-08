be7c6ae3f81773ecc202b2c41e27b2d61d6cd461
evanmorrow <evanmorrow@gmail.com>
2020-02-08 15:47:50 -0800

    committing to pull

--- ServiceApp/CustomerService.aspx
+++ ServiceApp/CustomerService.aspx
@@ -1,20 +1,37 @@
 ﻿<%@ Page Title="Customers" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CustomerService.aspx.cs" Inherits="ServiceApp.CustomerService" %>
 
 <asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
-<%--    <asp:form id="form1" runat="server">--%>
-        <h2>Customer Service Details</h2>
+    <%--    <asp:form id="form1" runat="server">--%>
+        <h2>Customer Service List</h2>
         <br />
         <div>
             <asp:Button ID="Button1" runat="server" Text="Add" />
         </div>
+        <hr />
         <div> 
-        <asp:ListView ID="ListView2" runat="server" OnSelectedIndexChanged="ListView1_SelectedIndexChanged">
+        <asp:ListView ID="ListView2" runat="server" OnSelectedIndexChanged="ListView1_SelectedIndexChanged" OnItemDataBound="ListView_ItemDataBound">
             <ItemTemplate>
-                <asp:Label runat="server" Text='<%# Eval("customerid") %>'> </asp:Label>
-                <asp:Label runat="server" Text='<%# Eval("ServiceTypeID") %>'></asp:Label> 
-                <asp:Label runat="server" Text='<%# Eval("ExpectedDuration") %>'></asp:Label> 
-                <br> </br>
-<%--                <asp:LinkButton ID="SelectButton" runat="server" CommandName="Select" Text='<% #Eval("CustomerID") %>' />  --%>        
+                <asp:Label runat="server" Text='<%# Eval("name") %>'> </asp:Label>
+                <asp:Label runat="server" Text='<%# Eval("certificationrqts") %>'></asp:Label> 
+                <asp:Label runat="server" Text='<%# Eval("rate") %>'></asp:Label> 
+                <asp:Label runat="server" Text='<%# Eval("expectedduration") %>'></asp:Label> 
+                <br/> 
+                <div id="toggleDiv" runat="server">
+                    <asp:Label ID="ServiceID" runat="server" Text='<%# "Address: " + Eval("servicetypeid") %>'></asp:Label> 
+                    <br />
+                    <asp:Label ID="CustomerServiceID" runat="server" Text='<%# "Manager ID: " + Eval("customerserviceid") %>'></asp:Label> 
+                    <br />
+                    <asp:Label ID="CustomerID" runat="server" Text='<%# "Certified For: " + Eval("customerid") %>'></asp:Label> 
+                </div>
+                <br/> 
+                <asp:Button ID="DetailsButton" runat="server" CommandName="DetailsButton" Text='Details' />
+                <asp:Button ID="UpdateButton" runat="server" CommandName="UpdateButton" Text='Edit' />
+                <asp:Button ID="DeleteButton" runat="server" CommandName="DeleteButton" CommandArgument='<%# BitConverter.ToString((byte[])Eval("customerserviceid")) %>' Text='Delete' />
+
+                <br />
+                <asp:Label ID="DeleteConfirm" runat="server" Text=''></asp:Label> 
+                <br/> <br />
+                <hr />
             </ItemTemplate>
         </asp:ListView>
      </div> 
--- ServiceApp/CustomerService.aspx.cs
+++ ServiceApp/CustomerService.aspx.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using System.Web.Security;
 using System.Collections;
@@ -13,20 +14,66 @@ namespace ServiceApp
 {
     public partial class CustomerService : System.Web.UI.Page
     {
+        private string connectionString = "DSN=myOracle;Uid=system;Pwd=oracle1";
         protected void Page_Init(object sender, EventArgs e)
         {
             ListView2.ItemCommand += new EventHandler<ListViewCommandEventArgs>(ListView1_ItemCommand);
         }
         void ListView1_ItemCommand(object sender, ListViewCommandEventArgs e)
-        {
-            //Literal1.Text = "You clicked the " + (String)e.CommandName + " button";
+        { 
+            // displays the details by setting div visibility to true
+            if (e.CommandName == "DetailsButton")
+            {
+                HtmlGenericControl div = (HtmlGenericControl)e.Item.FindControl("toggleDiv");
+                div.Visible = true;
+            }
+            if (e.CommandName == "UpdateButton")
+            {
+                try
+                {
+                    //OdbcConnection myConn = new OdbcConnection(connectionString);
+                    //myConn.Open();
+                    //string deleteQuery = "DELETE FROM CustomerService WHERE ID = ?";
+                    //    OdbcCommand command = new OdbcCommand(deleteQuery, myConn);
+                    //    string id = (string)e.CommandArgument;
+                    //    command.Parameters.Add("@ID", OdbcType.Text).Value = id.Replace("-", string.Empty);
+                    //    int result = command.ExecuteNonQuery();
+                    //    myConn.Close();
+
+                    //    Response.Redirect("CustomerService.aspx");
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err);
+                }
+            }
+            if (e.CommandName == "DeleteButton")
+            {
+                try
+                {
+                    OdbcConnection myConn = new OdbcConnection(connectionString);
+                    myConn.Open();
+                    string deleteQuery = "DELETE FROM CustomerService WHERE ID = ?";
+                    OdbcCommand command = new OdbcCommand(deleteQuery, myConn);
+                    string id = (string)e.CommandArgument;
+                    command.Parameters.Add("@ID", OdbcType.Text).Value = id.Replace("-", string.Empty);
+                    int result = command.ExecuteNonQuery();
+                    myConn.Close();
+
+                    Response.Redirect("CustomerService.aspx");
+                }
+                catch (Exception err)
+                {
+                    Console.WriteLine(err);
+                }
+            }
         }
         protected void Page_Load(object sender, EventArgs e)
         {
             string myConnection = "dsn=myOracle;uid=system;pwd=oracle1";
             OdbcConnection myConn = new OdbcConnection(myConnection);
             myConn.Open();
-            string mySelectQuery = "select utl_raw.cast_to_varchar2(UTL_RAW.cast_to_raw(CustomerID)) as CustomerID, utl_raw.cast_to_varchar2(UTL_RAW.cast_to_raw(ServiceTypeID)) as ServiceTypeID, ExpectedDuration from customerservice;";
+            string mySelectQuery = "select utl_raw.cast_to_varchar2(UTL_RAW.cast_to_raw(CustomerID)) as CustomerID, utl_raw.cast_to_varchar2(UTL_RAW.cast_to_raw(ServiceTypeID)) as ServiceTypeID, ExpectedDuration, customerservice.id as customerserviceid, servicetype.id as servicetypeid, name, certificationrqts, rate from customerservice join servicetype on customerservice.servicetypeid = servicetype.id;";
             OdbcCommand command = new OdbcCommand(mySelectQuery, myConn);
 
             if (!Page.IsPostBack)
@@ -35,6 +82,12 @@ namespace ServiceApp
                 ListView2.DataBind();
             }
             myConn.Close();
+        }
+        // hides the details
+        protected void ListView_ItemDataBound(object sender, ListViewItemEventArgs e)
+        {
+            HtmlGenericControl div = (HtmlGenericControl)e.Item.FindControl("toggleDiv");
+            div.Visible = false;
         }
 
         protected void ListView1_SelectedIndexChanged(object sender, EventArgs e)
