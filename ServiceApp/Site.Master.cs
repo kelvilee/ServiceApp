df22a603027f5d46f1ba38b501b4ff0ebe879455
Taylor <tlr.laan@gmail.com>
2020-02-10 12:01:00 -0800

    Added session tracker and logout button

--- ServiceApp/Site.Master.cs
+++ ServiceApp/Site.Master.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 
 namespace ServiceApp
@@ -15,6 +16,14 @@ namespace ServiceApp
             {
                 Response.Redirect("Login.aspx");
             }
+            UserLabel.DataBind();
+        }
+
+        protected void LogoutBtn_Click(object sender, EventArgs e)
+        {
+            Session["LoggedIn"] = "false";
+            Session["User"] = "";
+            Response.Redirect("Login.aspx");
         }
     }
 }
--- ServiceApp/Site.Master.designer.cs
+++ ServiceApp/Site.Master.designer.cs
@@ -15,6 +15,24 @@ namespace ServiceApp
     {
 
         /// <summary>
+        /// UserLabel control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label UserLabel;
+
+        /// <summary>
+        /// LogoutBtn control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button LogoutBtn;
+
+        /// <summary>
         /// MainContent control.
         /// </summary>
         /// <remarks>
